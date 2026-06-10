9102622a42194cfa0856d3cc595797927ae35a54
Alicja Rembisz <ala.rem56@gmail.com>
2026-06-10 20:00:46 +0200

    kill me

--- init-multiple-databases.sh
+++ init-multiple-databases.sh
@@ -10,6 +10,7 @@ function create_user_and_database() {
 EOSQL
 }
 
+
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
     echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
     for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
